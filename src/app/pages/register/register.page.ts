b32da765993ee687b9e5bba3cc6d822b1166d3f2
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-27 19:14:27 -0300

    refactor: adicionando validações e logica de envio do formulario de cadastro

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConductorService } from 'src/app/services/conductor/conductor.service';
+import { CpfValidationService } from 'src/app/services/cpf-validation/cpf-validation.service';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -12,56 +14,69 @@ export class RegisterPage implements OnInit {
   formRegister = {
     name: '',
     email: '',
+    cpf: '',
     password: ''
   }
 
-  InvalidEmailSpanError = false;
+  invalidEmailSpanError = false;
   requiredInputsError = false;
-  InvalidPasswordSpanError = false;
+  invalidPasswordSpanError = false;
+  invalidCPFSpanError = false;
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private conductorService: ConductorService, private router: Router, private validateCpf: CpfValidationService) { }
 
   ngOnInit() {
   }
 
+  resetForm() {
+    this.formRegister = {
+      name: '',
+      email: '',
+      cpf: '',
+      password: ''
+    }
+  }
+
   register() {
-    this.InvalidEmailSpanError = false;
+    // validacoes
+    this.invalidEmailSpanError = false;
     this.requiredInputsError = false;
-    this.InvalidPasswordSpanError = false;
+    this.invalidPasswordSpanError = false;
+    this.invalidCPFSpanError = false;
     if (
       this.formRegister.name === '' ||
       this.formRegister.email === '' ||
-      this.formRegister.password === ''
+      this.formRegister.password === '' ||
+      this.formRegister.cpf === ''
     ) {
       this.requiredInputsError = true;
       return;
     }
-    const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    const result: boolean = expression.test(this.formRegister.email);
-    if(!result) {
-      this.InvalidEmailSpanError = true;
+    if (!this.validateCpf.execute(this.formRegister.cpf)) {
+      this.invalidCPFSpanError = true;
+      return;
+    }
+    const expressionEmail: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const resultRegexEmail: boolean = expressionEmail.test(this.formRegister.email);
+    if(!resultRegexEmail) {
+      this.invalidEmailSpanError = true;
       return
     }
     if(this.formRegister.password.length < 6) {
-      this.InvalidPasswordSpanError = true
+      this.invalidPasswordSpanError = true
       return
     }
+
+    // submissao
     
-    this.userService.register(this.formRegister).subscribe(
+    this.conductorService.register(this.formRegister).subscribe(
       () => {
         this.router.navigate(['/login']);
-        this.formRegister = {
-          name: '',
-          email: '',
-          password: ''
-        }
+        this.resetForm()
       },
       (error: any) => {
-        this.formRegister = {
-          name: '',
-          email: '',
-          password: ''
-        }
+        this.resetForm()
+        console.log(error)
       }
     );
   }
