c6e347f99ab7c887928e2019791cac44ff5d12f0
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-01-30 16:18:11 -0300

    refactor: configurando paginas criadas no home routing

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -6,6 +6,31 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: 'students',
+        children:[
+          {
+            path: '',
+            loadChildren: () => import('../students/students.module').then(m => m.StudentsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'settings',
+        children:[
+          {
+            path: '',
+            loadChildren: () => import('../settings/settings-routing.module').then(m => m.SettingsPageRoutingModule)
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/students',
+    pathMatch: 'full'
   }
 ];
 
