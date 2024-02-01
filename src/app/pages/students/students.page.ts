208ca5fc5da2269a1b79a89521f114212a10b5f8
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-01 20:48:46 -0300

    refactor: adicionando selector para buscar estudantes no store na pagina de estudantes

--- src/app/pages/students/students.page.ts
+++ src/app/pages/students/students.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { address } from 'src/app/entities/address';
+import { transportationType } from 'src/app/entities/enums/transportationType';
+import { responsible } from 'src/app/entities/responsible';
+import { student } from 'src/app/entities/student';
+import { StudentService } from 'src/app/services/student/student.service';
+import { IAppState, loadNotesAction } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-students',
@@ -7,16 +15,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsPage implements OnInit {
 
-  students: any[] = []
 
-  constructor() { }
+  students$: Observable<student[]> = this.store.select('app').pipe(
+    map(e => e.students)
+  )
+
+  constructor(private store: Store<{app: IAppState}>, private studentService: StudentService) { }
 
   ngOnInit() {
   }
-
+  
   activeButton: string | null = "todos";
-
+  
   setActiveButton(button: string): void {
     this.activeButton = button === this.activeButton ? null : button;
   }
+  
 }
