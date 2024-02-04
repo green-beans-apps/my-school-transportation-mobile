df1f773d397495f69bf766f8ba8e5f4bc10c96b6
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-04 10:08:44 -0300

    refactor: adicionando arquivo ts da pagina de settings

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {  map } from 'rxjs';
+import { IAppState} from 'src/app/store/app.state';
+import { Router } from '@angular/router';
+import { resetActions } from 'src/app/store/resetActions';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
+
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  conductor$ = this.store.select('app').pipe(
+    map(e => e.conductor)
+  )
+
+  constructor(
+    private store: Store<{ app: IAppState }>,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  exit() {
+    this.store.dispatch(resetActions.reset());
+    this.router.navigate(['/login']);
   }
 
 }
