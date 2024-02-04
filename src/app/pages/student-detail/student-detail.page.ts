47c1ef279d42527b37f3db03b4417428556636fb
Danilo Desktop <danilo.ppessoa2610@gmail.com>
2024-02-04 15:27:21 -0300

    refactor: implementando logica para converter o enum para string

--- src/app/pages/student-detail/student-detail.page.ts
+++ src/app/pages/student-detail/student-detail.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, map, switchMap } from 'rxjs';
+import { shift } from 'src/app/entities/enums/shift';
+import { transportationType } from 'src/app/entities/enums/transportationType';
 import { student } from 'src/app/entities/student';
 import { IAppState } from 'src/app/store/app.state';
 
@@ -36,7 +38,47 @@ export class StudentDetailPage implements OnInit {
   private getStudentById(students: student[], studentId: string): Observable<student | undefined> {
     return this.store.select('app').pipe(
       map(e => e.students),
-      map(students => students.find(student => student.id === studentId))
+      map(students => students.find(student => student.id === studentId)),
+      map((foundStudent: student | undefined) => {
+        // Modificar o valor, se o estudante for encontrado
+        if (foundStudent) {
+          const transportationTypeLabel = this.getTransportationTypeLabel(foundStudent.transportationType);
+          const shiftLabel = this.getShiftLabel(foundStudent.shift);
+
+          // Retornar o valor modificado
+          return {
+            ...foundStudent,
+            transportationType: transportationTypeLabel,
+            shift: shiftLabel
+          };
+        }
+  
+        return foundStudent; // Retornar o valor sem modificar se não for encontrado
+      })
     );
+  }
+
+  private getTransportationTypeLabel(transportationType: string): string {
+    switch (transportationType) {
+      case "IDA_E_VOLTA":
+        return "Ida & Volta";
+      case "IDA":
+        return "Ida";
+      case "VOLTA":
+        return "Volta";
+      default:
+        return '';
+    }
+  }
+
+  private getShiftLabel(shift: string): string {
+    switch (shift) {
+      case "MANHA":
+        return "Manhã";
+      case "TARDE":
+        return "Tarde";
+      default:
+        return '';
+    }
   }
 }
