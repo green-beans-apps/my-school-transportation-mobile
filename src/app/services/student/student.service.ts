34182da7cfbc11c832fdcd4cb2e5e7047f88ad04
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-08 18:22:14 -0300

    refactor: criando metodo de registrar estudante no service de estudante

--- src/app/services/student/student.service.ts
+++ src/app/services/student/student.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { student } from 'src/app/entities/student';
+import { IRegisterStudentRequest } from './registerStudentRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,39 @@ export class StudentService {
 
     const urlGetAll = `${this.baseUrl}student/conductor/${conductorId}`
     return this.http.get<student[]>(urlGetAll, {headers: headers})
+  }
+
+  registerStudent(student: student): Observable<student> {
+    const requestData: IRegisterStudentRequest = {
+      student: {
+        studentName: student.name,
+        school: student.school,
+        grade: student.grade,
+        monthlyPayment: student.monthlyPayment,
+        monthlyPaymentExpiration: student.monthlyPaymentExpiration,
+        conductorId: window.localStorage.getItem('conductorId'),
+        shift: student.shift,
+        transportationType: student.transportationType,
+      },
+      responsible: {
+        responsibleName: student.responsible.name,
+        email: student.responsible.email,
+        phone: student.responsible.phone,
+      },
+      address: {
+        city: student.address.city,
+        district: student.address.district,
+        street: student.address.street,
+        referencePoint: student.address.referencePoint,
+        houseNumber: student.address.houseNumber
+      }
+    }
+
+    const headers = new HttpHeaders({
+      'Authorization': window.localStorage.getItem('token') ?? ''
+    })
+
+    const urlCreateStudent = `${this.baseUrl}student`
+    return this.http.post<student>(urlCreateStudent, requestData, {headers: headers})
   }
 }
