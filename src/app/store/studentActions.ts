9222e59a560b53ad811a7e80575823db5f12bb4c
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-08 21:20:05 -0300

    refactor: adicionando action de registrar pagamento

--- src/app/store/studentActions.ts
+++ src/app/store/studentActions.ts
@@ -1,14 +1,17 @@
 import { createAction, props } from "@ngrx/store"
 import { student } from "../entities/student"
+import { payment } from "../entities/payment"
 
 const loadStudentsAction = createAction("[App] [student] load students")
 const setStudentsAction = createAction("[App] [student] set students", props<{students: student[]}>())
 const successSetStudentAction = createAction("[App] [Student] success set students")
 const registerStudentAction = createAction("[App] [Student] register student", props<{student: student}>())
+const registerPaymentAction = createAction("[App] [Student] [Payment] register Payment", props<{payment: payment, studentId: string}>())
 
 export const studentActions = {
   loadStudentsAction,
   setStudentsAction,
   successSetStudentAction,
-  registerStudentAction
+  registerStudentAction,
+  registerPaymentAction
 }
