79f167cc1e45e482db69f2b4d614e87a12a078aa
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-09 20:42:41 -0300

    refactor: transformando propriedades do estudante em enum no loadStudentsEffect

--- src/app/store/student-effects.service.ts
+++ src/app/store/student-effects.service.ts
@@ -4,7 +4,9 @@ import { Store } from '@ngrx/store';
 import { IAppState } from './app.state';
 import { studentActions } from './studentActions';
 import { StudentService } from '../services/student/student.service';
-import { exhaustMap, map, switchMap, tap } from 'rxjs';
+import { catchError, exhaustMap, map, of, switchMap, tap } from 'rxjs';
+import { convertStringToShift } from '../utils/convertStringToShift';
+import { convertStringToTransportationType } from '../utils/convertStringToTransportationType';
 
 @Injectable({
   providedIn: 'root'
@@ -17,16 +19,23 @@ export class StudentEffectsService {
     private store: Store<{ app: IAppState }>
   ) { }
 
-  public loadStudentsEffect$ = createEffect(() => this.actions$.pipe(
+  loadStudentsEffect$ = createEffect(() => this.actions$.pipe(
     ofType(studentActions.loadStudentsAction),
     switchMap(() =>
-      this.studentService.getAll()
+      this.studentService.getAll().pipe(
+        map((students) => students.map(student => ({
+          ...student,
+          shift: convertStringToShift(student.shift),
+          transportationType: convertStringToTransportationType(student.transportationType)
+        }))),
+        catchError(error => of([]))
+      )
     ),
     tap((students) =>
-      this.store.dispatch(studentActions.setStudentsAction({ students: students }))
+      this.store.dispatch(studentActions.setStudentsAction({ students }))
     ),
     map(() => studentActions.successSetStudentAction())
-  ))
+  ));
 
   public registerStudentEffect$ = createEffect(() => this.actions$.pipe(
     ofType(studentActions.registerStudentAction),
