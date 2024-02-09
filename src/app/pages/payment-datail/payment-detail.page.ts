a539778706925c2944a186c87a5304cec4e760f2
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-08 21:15:32 -0300

    refactor: adicionando metodo de registrar pagamento na pagina payment detail

--- src/app/pages/payment-datail/payment-detail.page.ts
+++ src/app/pages/payment-datail/payment-detail.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, map, switchMap } from 'rxjs';
+import { map } from 'rxjs';
 import { months } from 'src/app/entities/enums/months';
 import { payment } from 'src/app/entities/payment';
-import { student } from 'src/app/entities/student';
 import { IAppState } from 'src/app/store/app.state';
 import { studentActions } from 'src/app/store/studentActions';
+import { v4 as uuidv4 } from 'uuid';
 
 interface IPaymentRender {
   id: string | undefined;
@@ -28,7 +28,7 @@ export class PaymentDetailPage implements OnInit {
 
   student$ = this.store.select('app').pipe(
     map(e => e.students),
-    switchMap(students => this.getStudentById(students, this.studentId))
+    map(students => students.find(student => student.id === this.studentId))
   )
   
   constructor(
@@ -43,21 +43,22 @@ export class PaymentDetailPage implements OnInit {
 
     this.student$.subscribe(student => {
       if (student) {
-        console.log(student.payments)
         this.paymentsRender = this.generatePaymentsRender(student.payments, student.monthlyPayment);
       }
     });
   }
 
-  returnPage() {
-    this.router.navigate(['/student-detail', this.studentId]);
+  registerPayment(month: months) {
+    const payment: payment = {
+      id: uuidv4(),
+      paymentDate: this.formatDateToDDMMYYYY(new Date()),
+      paymentMonth: month
+    }
+    this.store.dispatch(studentActions.registerPaymentAction({studentId: this.studentId, payment: payment}))
   }
 
-  private getStudentById(students: student[], studentId: string): Observable<student | undefined> {
-    return this.store.select('app').pipe(
-      map(e => e.students),
-      map(students => students.find(student => student.id === studentId))
-    );
+  returnPage() {
+    this.router.navigate(['/student-detail', this.studentId]);
   }
 
   private generatePaymentsRender(payments: payment[] | undefined, monthlyPayment: number): IPaymentRender[] {
@@ -75,5 +76,13 @@ export class PaymentDetailPage implements OnInit {
     }
   
     return renderedPayments;
+  }
+
+  formatDateToDDMMYYYY(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Os meses começam do zero
+    const year = String(date.getFullYear());
+  
+    return `${day}/${month}/${year}`;
   }
 }
