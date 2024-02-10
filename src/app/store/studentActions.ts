3e7f743c8c84792f11b5f69223761672e8a3f991
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-10 12:41:55 -0300

    refactor: criando actions de atualizacao de estudante, responsável e endereco

--- src/app/store/studentActions.ts
+++ src/app/store/studentActions.ts
@@ -1,17 +1,27 @@
 import { createAction, props } from "@ngrx/store"
 import { student } from "../entities/student"
 import { payment } from "../entities/payment"
+import { address } from "../entities/address"
+import { transportationType } from "../entities/enums/transportationType"
+import { shift } from "../entities/enums/shift"
 
 const loadStudentsAction = createAction("[App] [student] load students")
 const setStudentsAction = createAction("[App] [student] set students", props<{students: student[]}>())
 const successSetStudentAction = createAction("[App] [Student] success set students")
 const registerStudentAction = createAction("[App] [Student] register student", props<{student: student}>())
 const registerPaymentAction = createAction("[App] [Student] [Payment] register Payment", props<{payment: payment, studentId: string}>())
+const updateResponsibleAction = createAction("[App] [Student] [Responsible] update Responsible", props<{name: string, email: string, phone: string, studentId: string}>())
+const updateAddressAction = createAction("[App] [Student] [Address] update Address", props<{city: string, district: string, street: string, houseNumber: number, referencePoint: string, studentId: string}>())
+const updateStudentAction = createAction("[App] [Student] update student", props<{name: string, school: string, grade: string, transportationType: transportationType, shift: shift, monthlyPayment: number, monthlyPaymentExpiration: number, id: string}>())
+
 
 export const studentActions = {
   loadStudentsAction,
   setStudentsAction,
   successSetStudentAction,
   registerStudentAction,
-  registerPaymentAction
+  registerPaymentAction,
+  updateResponsibleAction,
+  updateAddressAction,
+  updateStudentAction
 }
