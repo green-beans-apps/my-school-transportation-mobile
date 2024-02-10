ca68cf9e5e48a212d19167554f5a162b33b51e27
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-10 12:40:47 -0300

    feat: criando tipo da requisicao de tualizar estudante

---
+++ src/app/services/student/updateStudentRequest.ts
@@ -0,0 +1,10 @@
+export interface  updateStudentRequest {
+  name: string;
+  school: string;
+  grade: string;
+  shift: string;
+  transportationType: string;
+  monthlyPayment: number;
+  monthlyPaymentExpiration: number;
+  studentId: string;
+}
