19a4106d4dcc6f2af1ca2111b09d4f311248072b
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-11 20:04:36 -0300

    refactor: adicionando metodo para direcionar paga a pagina de atualizacao do condutor na pagina de settings

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -5,6 +5,7 @@ import { IAppState} from 'src/app/store/app.state';
 import { Router } from '@angular/router';
 import { resetActions } from 'src/app/store/resetActions';
 import { ConductorService } from 'src/app/services/conductor/conductor.service';
+import { conductortActions } from 'src/app/store/conductorActions';
 
 @Component({
   selector: 'app-settings',
@@ -25,21 +26,18 @@ export class SettingsPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    const conductorId = localStorage.getItem("conductorId");
-
-    if (conductorId !== null) {
-      this.conductorService.getConductorById(conductorId).subscribe(response => {
-        this.conductor$ = of(response);
-      });
-    }
-}
-
+    this.store.dispatch(conductortActions.loadConductorAction())
+  }
 
   exit() {
     localStorage.clear();
     this.store.dispatch(resetActions.reset());
     window.localStorage.clear()
     window.location.reload()
+  }
+
+  redirectUpdateConductor() {
+    this.router.navigate(['/update-conductor'])
   }
 
 }
