2e1a70c4eeac955b58a688be8466fea040dada3d
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-11 20:05:03 -0300

    refactor: removendo services sem uso

--- src/app/store/conductorAdtions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from "@ngrx/store"
-import { conductor } from "../entities/conductor"
-
-export const setConductorAction = createAction("[App] [conductor] set conductor", props<{conductor: conductor}>())
-
-export const conductortActions = {
-  setConductorAction
-}
