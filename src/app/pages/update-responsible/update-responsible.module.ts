36383050f5e52043edead54c599d9e29752ac88d
Cristiano-woody <cristiano.nog.woody@gmail.com>
2024-02-13 00:28:37 -0300

    refactor: adicionando modulo do componente de popup no modulo de update responsavel

--- src/app/pages/update-responsible/update-responsible.module.ts
+++ src/app/pages/update-responsible/update-responsible.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { UpdateResponsiblePageRoutingModule } from './update-responsible-routing.module';
-
 import { UpdateResponsiblePage } from './update-responsible.page';
 import { MaskitoDirective } from '@maskito/angular';
+import { GenericModalDialogModule } from 'src/app/components/generic-modal-dialog/generic-modal-dialog.module';
 
 @NgModule({
   imports: [
@@ -16,7 +14,8 @@ import { MaskitoDirective } from '@maskito/angular';
     IonicModule,
     UpdateResponsiblePageRoutingModule,
     ReactiveFormsModule,
-    MaskitoDirective
+    MaskitoDirective,
+    GenericModalDialogModule
   ],
   declarations: [UpdateResponsiblePage]
 })
